d76aac2392f94bf467f476da81d6c653d73291c8
Thiago Laurence <francolaurence47@gmail.com>
2025-03-21 14:34:51 -0300

    exercise(CI/CD - github actions): implementacion de ejercicio 11.9 - Cypress

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'jest/globals': true
+    'jest/globals': true,
+    'cypress/globals': true
   },
   'extends': [
     'eslint:recommended',
@@ -21,7 +22,7 @@ module.exports = {
     }
   },
   'plugins': [
-    'react', 'jest'
+    'react', 'jest', 'cypress'
   ],
   'rules': {
     'indent': [
