dd5aa02dd4071bfe462ce9d0646d6fcb2abad722
Thiago Laurence <francolaurence47@gmail.com>
2025-03-22 10:51:54 -0300

    fix(env-PORT)

--- cypress/e2e/PokemonPage.cy.js
+++ cypress/e2e/PokemonPage.cy.js
@@ -1,6 +1,8 @@
+import process from 'process'
+const PORT = process.env.PORT || 5000
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit(`http://localhost:${PORT}`)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
--- e2e-test/PokemonPage.spec.js
+++ e2e-test/PokemonPage.spec.js
@@ -1,8 +1,10 @@
+import process from 'process'
 import { test, describe, expect } from '@playwright/test'
+const PORT = process.env.PORT || 5000
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080')
+    await page.goto(`http://localhost:${PORT}`)
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
